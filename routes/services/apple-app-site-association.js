e4f0ac04a1abddb987e45a267304bfcbb7cb2c3a
guanghshuai.li <guangshuai.li@innocellence.com>
2016-07-08 11:27:31 +0800

    ios

---
+++ routes/services/apple-app-site-association.js
@@ -0,0 +1,35 @@
+
+var fs = require('fs');
+
+var USER_PATH = './apple-app-site-association';
+
+var Test = {
+  init: function(app){
+    app.get('/apple-app-site-association', this.doShow);
+  },
+
+  // doTest: function(req, res){
+  //   res.send({
+  //     status: 1,
+  //     info: '测试服务doTest'
+  //   });
+  // },
+
+  doShow: function(req, res){
+
+      fs.readFile(USER_PATH, function(err, data){
+      if(!err){
+
+        var obj = JSON.parse(data);
+
+        return res.send({
+            status: 1,
+            data: obj
+          });
+        
+      }
+    });
+  }
+};
+
+module.exports = Test;
